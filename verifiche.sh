4c41ae4f33f7c630b2cbc93d8bfe95536bdff74c
mariaranci <31917865+mariaranci@users.noreply.github.com>
2021-01-05 11:57:11 +0100

    Update verifiche.sh
    tolte verifiche di compilazione interne al DBmeteo perché eseguite in Grafana

--- verifiche.sh
+++ verifiche.sh
@@ -7,11 +7,12 @@
 #     scrive su file eventuali discrepanze con quello che risulta al DBmeteo
 #     e carica il file su Minio
 #
+################  non eseguito perchè sostituito da dashboard di grafana #########
+#
 # 3.  esegue verifiche sulla compilazione dei campi di anagrafica nel DBmeteo
 #     scrive su file eventuali lacune
 #     e carica il file su Minio
 #
-###############  non eseguito perchè sostituito da dashboard di grafana #########
 # 4.  esegue verifiche sui sensori inviati a PC
 #     scrive su file eventuali discrepanze rispetto agli invii attesi
 #     e carica il file su Minio
@@ -87,27 +88,27 @@ do
    
    ################# 3 #################################
    
-   Rscript A_verifiche.R 
+  # Rscript A_verifiche.R 
    
    # verifico se è andato a buon fine
-   STATO=$?
-   echo "STATO USCITA SCRIPT ====> "$STATO
+   #STATO=$?
+   #echo "STATO USCITA SCRIPT ====> "$STATO
 
-   if [ "$STATO" -eq 1 ] # se si sono verificate anomalie esci 
-   then
-       exit 1
-   else # caricamento su MINIO 
-       $S3CMD put verifiche.out s3://rete-monitoraggio 
+   #if [ "$STATO" -eq 1 ] # se si sono verificate anomalie esci 
+   #then
+   #    exit 1
+   #else # caricamento su MINIO 
+   #    $S3CMD put verifiche.out s3://rete-monitoraggio 
        
-       # controllo sul caricamento su MINIO 
-       if [ $? -ne 0 ]
-       then
-         echo "problema caricamento su MINIO"
-         exit 1
-       fi
-   fi
+   #    # controllo sul caricamento su MINIO 
+   #    if [ $? -ne 0 ]
+   #    then
+   #      echo "problema caricamento su MINIO"
+   #      exit 1
+   #    fi
+   #fi
     
-   rm -f verifiche.out
+   #rm -f verifiche.out
    
    ################# 3 #################################
    
